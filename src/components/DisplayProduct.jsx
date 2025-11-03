f43f8f9817206cc85e7c2c0e8ac9c6b42013da0f
Ayush <ayush@Ayushs-MacBook-Air.local>
2025-11-03 19:05:31 +0530

    VercelBug

--- src/components/DisplayProduct.jsx
+++ src/components/DisplayProduct.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import ProductCard from "./productCard";
+import ProductCard from "./ProductCard";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
